39d71a10988fd2e8941c8aa40b9035c73d34ac5c
Survesh Rathore <113424413+SurveshRathore@users.noreply.github.com>
2023-05-04 10:03:20 +0530

    Create spScheduleAppointment.sql

---
+++ sql/spScheduleAppointment.sql
@@ -0,0 +1,87 @@
+use hospitalCare
+
+create table AppointmentTable
+(
+	AppointmentId int primary key identity(1,1),
+	AppointmentDate DateTime,
+	AppointmentType varchar(255),
+	AppointmentDescription varchar(255),
+	AppointmentStatus varchar(255) default 'Pending',
+	PatientId int foreign key references PatientTable(PatientId),
+	doctorId int foreign key references DoctorTable(doctorId)
+)
+create procedure spScheduleAppointment
+(
+	@AppointmentDate DateTime,
+	@AppointmentType varchar(255),
+	@AppointmentDescription varchar(255),
+	@PatientId varchar(255),
+	@doctorId varchar(255)
+)
+AS
+SET XACT_ABORT on;
+BEGIN
+	Begin Try
+		Begin Transaction
+			Declare @Result varchar(255) = '';
+				if exists (select * from DoctorTable where doctorId = @doctorId)
+				BEGIN
+					if exists (select * from PatientTable where PatientId = @PatientId)
+						begin
+							insert into AppointmentTable (AppointmentDate, AppointmentType, AppointmentDescription, PatientId, doctorId) 
+							values (@AppointmentDate, @AppointmentType, @AppointmentDescription, @PatientId, @doctorId)
+
+							select * from DoctorTable
+
+							Set @Result = 'doctor details inserted successfully';
+						
+						end
+					else
+					begin
+						Set @Result = 'Patient Id not Exists';
+						THROW 52000, 'Patient Id not Exists', -1;
+					end
+				END
+
+				else
+				begin
+					Set @Result = 'Doctor Id not Exists';
+					THROW 52000, 'Doctor Id not Exists', -1;
+				end
+
+			commit Transaction
+
+			return @Result
+	End Try
+	Begin Catch
+		
+		if(XACT_STATE()) = -1
+		begin
+				print
+					'Transaction is non-commitable' + ' Rolling Back Transaction'
+					RollBack Transaction;
+					Print @Result;
+					return @result;
+		end
+
+		Else if(XACT_STATE()) = 1
+		Begin
+			Print
+				'Transaction is Complitable' + 'Commiting Back Transaction'
+				COMMIT TRANSACTION;
+				Print @Result;
+				return @result;
+		End;
+		--print 'Unable to Add doctor'
+	End Catch
+END
+
+exec spScheduleAppointment
+
+	@AppointmentDate = '28 april 2023 01 PM',
+	@AppointmentType = 'abc',
+	@AppointmentDescription = 'desc',
+	@PatientId = 1,
+	@doctorId = 1
+
+	--drop proc spAddDoctor
